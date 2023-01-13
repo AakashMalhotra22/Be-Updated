b6d971294f7369bf6dcca529174e69aa8d9b86f2
aakashmalhotra22 <aakashmalhotra2863@gmail.com>
2023-01-13 12:51:48 +0530

    creating react app and pushing changes

---
+++ src/Components/News.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>
+        This is a news component
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>c
+        
+      </div>
+    )
+  }
+}
